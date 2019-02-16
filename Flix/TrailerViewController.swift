3f5092ef91c4c1e7e2a72ebfde811dd5cd220464
Michael Zetune <michael@zetune.com>
2019-02-16 11:40:09 -0600

    Some trailer work

--- Flix/TrailerViewController.swift
+++ Flix/TrailerViewController.swift
@@ -26,25 +26,34 @@ class TrailerViewController: UIViewController, WKUIDelegate {
         super.viewDidLoad()
         
         let movieId = movie["id"] as! Int
-        // need to unpack the JSON returned by the link below
-        let movieTrailerUrl =
-            URL(string: "https://api.themoviedb.org/3/movie/\(movieId)/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US")
-        print(movieTrailerUrl)
-        let request = URLRequest(url: movieTrailerUrl!)
-        webView.load(request)
+        print("MOVIEID: \(movieId)")
+        let youtubeBaseUrl = "https://www.youtube.com/watch?v="
+        
+        let requestUrl = URL(string: "https://api.themoviedb.org/3/movie/\(movieId)/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US")!
+        let request = URLRequest(url: requestUrl, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print("DATADICTIONARY: \(dataDictionary)")
+                let videos = dataDictionary["results"] as! [[String: Any]]
+                print("VIDEOS: \(videos)")
+                let trailerVideo = videos[0]
+                print("TRAILERVIDEO: \(trailerVideo)")
+                let trailerVideoKey = trailerVideo["key"]!
+                print("TRAILERVIDEOKEY: \(trailerVideoKey)")
+                let trailerRequestUrl = URL(string: "\(youtubeBaseUrl)+ \(trailerVideoKey)")!
+                let trailerRequest = URLRequest(url: trailerRequestUrl, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+                self.webView.load(trailerRequest)
+            }
+        }
+        task.resume()
+        
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
